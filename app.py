393a6c1661684683ec8db2a47c9c260f82b57f19
Angel <27angelrani@gmail.com>
2023-08-03 12:19:59 +0530

    loading the data

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('hello world')
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+data_url = "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series"
+df1 = pd.read_csv('data_url')
+df1
+
